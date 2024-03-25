33d760aa4399a3f6dac531acc8bda5b65e5b240c
Idemudia <agbonghaeidemudia@gmail.com>
2024-03-25 14:33:12 +0100

    fix: Modify app.js to show routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
+const notFound = require('./middleware/notFound')
 
 
 //middleware
@@ -12,6 +13,11 @@ app.use(express.json());
 
 app.use('/api/v1/tasks', tasks);
 
+//app.get('/api/v1/tasks')                  -get all tasks
+//app.post('/api/v1/tasks')                 -create all tasks
+//app.get('/api/v1/tasks/:id')              -get single task
+//app.patch('/api/v1/tasks/:id')            -update tasks
+//app.delete('/api/v1/tasks/:id')           -get all tasks
 
 const PORT = process.env.PORT || 3000;
 
